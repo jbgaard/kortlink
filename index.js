7f65c544e0a5387f5291f74595478b1c36f2d3a4
famasmaster78 <jxras11@gmail.com>
2020-09-10 15:24:37 +0200

    Test mysql connection

--- index.js
+++ index.js
@@ -9,6 +9,19 @@ const mysql = require("mysql");
 const express = require("express");
 const app = express();
 
+
+// Mysql Connection info
+var connection = mysql.createConnection({
+  host     : '188.114.165.148',
+  user     : 'jonas',
+  password : 'mBuaj28181',
+  database : 'kortlinkdb'
+});
+
+
+// Make connection to Mysql DB
+connection.connect();
+
 // Test express
 app.get("/", function (req, res){
 
@@ -23,7 +36,18 @@ app.get("/:kortlink", function (req, res){
 	// Var til kortlink som er efterspurgt
 	var kortlinkGet = req.params.kortlink;
 
-	res.send(`KortlinkNode - Andet - ${kortlinkGet}`);
+	// Test MYSQL
+	connection.query(`SELECT * FROM kortlink WHERE id="${{kortlinkGet}}"`, function (error, results, fields) {
+	  if (error) throw error;
+	  console.log(results[0]);
+
+	  var sqlResult = results[0];
+
+	});
+
+	connection.end();
+
+	res.send(`KortlinkNode - Andet - ${sqlResult}`);
 	console.log(`Get modtaget! - Andet - ${kortlinkGet}`);
 
 });
